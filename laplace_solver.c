c7c38a91730cb777e8e2abcb96ac7d8b209e941e
Nicholas Morrow <nickhudspeth@gmail.com>
2015-11-13 00:28:03 -0800

    Added laplace_solver.c

---
+++ laplace_solver.c
@@ -0,0 +1,42 @@
+/************************************************************************
+Title:    Laplace Solver
+Author:   Nicholas Morrow <nickhudspeth@gmail.com> http://www.nickhudspeth.com
+File:     laplace_solver.c
+Software: none
+Hardware: x86
+License:  GNU General Public License
+Usage:    Refer to the header file laplace_solver.h for a description of the routines.
+    See also example test_laplace_solver.c, if available.
+LICENSE:
+    Copyright (C) 2015 Nicholas Morrow
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+************************************************************************/
+
+/**********************    INCLUDE DIRECTIVES    ***********************/
+
+ #include "laplace_solver.h"
+
+
+/*********************    CONSTANTS AND MACROS    **********************/
+
+
+/***********************    GLOBAL VARIABLES    ************************/
+
+
+/*******************    FUNCTION IMPLEMENTATIONS    ********************/
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
